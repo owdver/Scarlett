07b4d9e146d89c422c32a5a0a579adcc7138f6c1
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-10-31 20:17:35 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https:/t.me/OB_LINKS')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/OB_LINKS')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
