b717c75370114ebc4351fe39fdbb36db4b3dc2da
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-11-01 18:16:32 +0530

    Update connection.py

--- plugins/connection.py
+++ plugins/connection.py
@@ -5,7 +5,9 @@ from info import ADMINS
 
 @Client.on_message((filters.private | filters.group) & filters.command('connect'))
 async def addconnection(client,message):
-    userid = message.from_user.id
+    userid = message.reply_to_message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"Yᴏᴜ Aʀᴇ Aɴᴏɴʏᴍᴏᴜs Aᴅᴍɪɴ. Usᴇ /connect {message.chat.id} Iɴ Pᴍ")
     chat_type = message.chat.type
 
     if chat_type == "private":
@@ -74,7 +76,9 @@ async def addconnection(client,message):
 
 @Client.on_message((filters.private | filters.group) & filters.command('disconnect'))
 async def deleteconnection(client,message):
-    userid = message.from_user.id
+    userid = message.reply_to_message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"Yᴏᴜ Aʀᴇ Aɴᴏɴʏᴍᴏᴜs Aᴅᴍɪɴ. Usᴇ /connect {message.chat.id} Iɴ Pᴍ"")
     chat_type = message.chat.type
 
     if chat_type == "private":
